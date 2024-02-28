08b2fcbc957b4ed9e332b084faf3c3ef58299c6d
Sanidhya Jain <sanidhyajain45@gmail.com>
2024-02-28 14:21:11 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    // all crud database methods
+}
